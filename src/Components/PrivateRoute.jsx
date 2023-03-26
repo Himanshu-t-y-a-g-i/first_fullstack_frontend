84a0fa383b47425e7dbeafead0e49f9f04ee65ab
Himanshu tyagi <himanshutyagi0158@gmail.com>
2023-03-26 17:26:00 +0530

    update

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -6,13 +6,13 @@ export const PrivateRoute = ({ children }) => {
     console.log(children.type.name);
     console.log(children);
     const store = useSelector(store => store.authReducer);
-    if (store.role === "admin" && children.type.name === "Admin" && store.isAuth === true) {
+    if (store.role === "admin" && (children.type.name === "Admin" || children.type.name === "gu") && store.isAuth === true) {
         return children;
     }
-    if ((store.role === "admin" || store.role === "seller" || store.role === "admin") && children.type.name === "DetailProduct" && store.isAuth === true) {
+    if ((store.role === "admin" || store.role === "seller" || store.role === "admin") && (children.type.name === "DetailProduct" || children.type.name === "yu") && store.isAuth === true) {
         return children;
     }
-    if ((store.role === "admin" || store.role === "seller") && children.type.name === "ProductEdit" && store.isAuth === true) {
+    if ((store.role === "admin" || store.role === "seller") && (children.type.name === "ProductEdit" || children.type.name === "hc") && store.isAuth === true) {
         return children;
     }
     if ((store.role === "admin" || store.role === "seller") && children.type.name === "SingleProductEdit" && store.isAuth === true) {

bdf869b4a34996089465c99f8938c8b3b8a97fe0
Himanshu tyagi <himanshutyagi0158@gmail.com>
2023-03-26 17:55:19 +0530

    update

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -8,7 +8,7 @@ export const PrivateRoute = ({ children }) => {
     if (store.role === "admin" && (children.type.name === "Admin" || children.type.name === "gu") && store.isAuth === true) {
         return children;
     }
-    if ((store.role === "admin" || store.role === "seller" || store.role === "admin") && (children.type.name === "DetailProduct" || children.type.name === "yu") && store.isAuth === true) {
+    if ((store.role === "admin" || store.role === "seller" || store.role === "user") && (children.type.name === "DetailProduct" || children.type.name === "yu") && store.isAuth === true) {
         return children;
     }
     if ((store.role === "admin" || store.role === "seller") && (children.type.name === "ProductEdit" || children.type.name === "hc") && store.isAuth === true) {
